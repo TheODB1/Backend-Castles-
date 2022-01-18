8c250a9ab339027a44f15f5ac0e16e2b4b588f48
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-01-19 00:17:50 +0100

    upload image fuction succeeded return a URL

--- routes/uploadsRouter.js
+++ routes/uploadsRouter.js
@@ -1,11 +1,10 @@
-import { Router } from "express";
-import imageUploader from "../middlewares/imageUploader.js";
-import verifyToken from "../middlewares/verifyToken.js";
+import { Router } from 'express';
+import verifyToken from '../middlewares/verifyToken.js';
+import imageUploader from '../middlewares/imageUploader.js';
+import { uploadResponse } from '../controllers/uploads.js';
 
 const uploadsRouter = Router();
 
-uploadsRouter.post("/", verifyToken, imageUploader.single('profile_pic'), (req, res) => {
-    res.send('upload');
-});
+uploadsRouter.post('/', verifyToken, imageUploader.single('image'), uploadResponse);
 
 export default uploadsRouter;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-import express from "express";
-import cors from "cors";
 import "dotenv/config.js";
 import "./db/client.js";
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
 import castleRouter from "./routes/castleRouter.js";
 import userRouter from "./routes/userRouter.js";
-import errorHandler from "./middlewares/errorHandler.js";
 import uploadsRouter from "./routes/uploadsRouter.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const publicDir = join(__dirname, 'public/uploads');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,11 +21,12 @@ app.get("/", (req, res) => {
 });
 
 app.use(cors({ origin: "*" }));
+app.use(express.static(publicDir));
 app.use(express.json());
 app.use("/api/castles", castleRouter);
 app.use("/auth", userRouter);
 app.use("/uploads", uploadsRouter);
-
+app.use('*', (req, res) => res.send('API'));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Serve at http://localhost:${port}`));
