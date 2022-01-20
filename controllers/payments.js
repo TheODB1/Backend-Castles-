1c6cb113b8a6ca68a1ef0f691059a0ef3864c0bf
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-01-20 16:44:47 +0100

    checkout, uploading castle works

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,7 +1,6 @@
 import stripe from "stripe";
+import { stripHtml } from "string-strip-html";
 import asyncHandler from "../middlewares/asyncHandler.js";
-
-const YOUR_DOMAIN = "http://localhost:3000";
 
 export const createCheckoutSession = asyncHandler(async (req, res) => {
   const {
@@ -15,13 +14,13 @@ export const createCheckoutSession = asyncHandler(async (req, res) => {
         amount: price * 100,
         name,
         currency: "eur",
-        description,
+        description: stripHtml(description).result,
         quantity: 1,
       },
     ],
     mode: "payment",
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    success_url: `${process.env.DREAM_CASTLE_FRONT}/confirm-order`,
+    cancel_url: `${process.env.DREAM_CASTLE_FRONT}/cancel-order`,
   });
   res.json({ checkoutURL: session.url });
 });
